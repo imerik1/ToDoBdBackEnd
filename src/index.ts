d075451b82486936dfeac4cf89dea63580fb7c13
Erik Marques <erikmarques@sisconsultoria.com.br>
2022-03-06 05:05:56 -0300

    feat: index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import env from "config/env";
+import userRouter from "controllers/UserController";
 import express from "express";
+import NodeCache from "node-cache";
 
 const app = express();
 const PORT = env.PORT;
+export const cache = new NodeCache({
+	checkperiod: 60,
+	stdTTL: 604800,
+	deleteOnExpire: true,
+	maxKeys: 100,
+});
+
+app.use(express.json());
+app.use("/users", userRouter);
 
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
 
